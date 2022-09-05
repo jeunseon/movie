62e576bc64b9a7af5297723656ccb9bb12d3a696
jeunseon <euns420@naver.com>
2022-09-05 13:06:08 +0900

    영화 데이터 불러오기

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
-import './App.css';
+import React from 'react';
+import axios from 'axios';
+import Movie from './Movie';
+import './App.scss'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>영화앱 만들기</h1>
-    </div>
-  );
+class App extends React.Component {
+  state = {isLoading : true, // render 위는 root 개념
+    movies: [],
+  } 
+  getMoives = async ()=>{
+    const {
+      data:{
+        data:{movies},
+      },
+    } = await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating');
+    // console.log(movies);
+    this.setState({movies, isLoading: false});
+  }
+  componentDidMount(){
+    this.getMoives();
+  }
+  render() {
+    const {isLoading, movies} = this.state;
+    return (
+      <section className='container'>
+        {isLoading ? (<div className='loader'>
+            <span className='loader_text'>'Loading...'</span>
+          </div>) : (
+          <div className='movies'>
+            {movies.map((movie)=>(
+              <Movie key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image} genres={movie.genres} />
+            ))}
+          </div>)
+        }
+      </section>
+    );
+  }
 }
 
 export default App;
